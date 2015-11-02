4a537a43ec58451367c71d364e8d989ff632ab64
Yusuf Abdulla Shunan <shunan@maldicore.com>
2015-11-02 15:31:46 +0500

    quickaddnew

---
+++ code/modeladmins/AdminTravelAgent.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* Common Data
+* Common Data for Travel Agent
+*/
+class AdminTravelAgent extends ModelAdmin {
+	private static $managed_models = array('Atolls','Islands','Categories','Tags','HolidayTypes','TransferTypes','Countries','BookingRequests');
+	private static $url_segment = 'ta_common';
+	private static $menu_title = 'Travel Agent Data';
+	private static $menu_priority = 1;
+}
+
+ ?>
